cff4797f54f3bf827a1b910d650ab5ad0fbfd4fc
fabio menezes <fabiomenezes@admedia.pt>
2022-01-12 23:05:45 +0000

    first commit!

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dul N'Nouk White</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link  rel="stylesheet" href="css/index.css"/>
+    <link rel="icon" type="image/png" href="favicon.png" />
+</head>
+<body>
+    <header>
+
+    </header>
+    <main class="container">
+        <article>
+            <h1>Dul N'Nouk White</h1>
+            <p>Dreamers love music.</p>
+        </article>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
