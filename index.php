2eaf7c8ceef5211a1625254fffff69d483760c55
fabio menezes <fabiomenezes@admedia.pt>
2022-01-28 01:34:33 +0000

    first commit!

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php
 $cache = time();
-?>
-<!DOCTYPE html>
-<html lang="en">
+?><!DOCTYPE html>
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     <title>Dul N'Nouk White</title>
+    <meta name="description" content="Os Dul and Nouk White são uma banda de rock alternativo madeirense com um repertório constituído, maioritariamente, por temas originais que visitam géneros musicais bastante distintos como o rock, o fado, o blues, o pop, o jazz e a música do mundo.">
+    <meta name="keywords" content="dul organic music madeira rock pop portugal">
+    <meta name="author" content="dul and nouk white">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link  rel="stylesheet" href="css/index.css?<?php echo $cache; ?>"/>
+    <link  rel="stylesheet" href="/css/index.css?<?php echo $cache; ?>"/>
     <link rel="icon" type="image/jpg" href="img/favicon.jpg" />
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
-<header>
-
-</header>
 <main>
+    <header>
+        <img src="img/fundo.jpg" alt="Dul N'Nouk White"/>
+    </header>
     <article>
-        <h1>Dul and Nouk White</h1>
+        <h1>dul and nouk white</h1>
         <p>
             Os Dul and Nouk White são uma banda de rock alternativo madeirense com um repertório constituído, maioritariamente, por temas originais que visitam géneros musicais bastante distintos como o rock, o fado, o blues, o pop, o jazz e a música do mundo. Contudo, a roupagem envolto a este repertório eclético faz com que se agreguem algumas características sonoras que vão dar origem à sonoridade peculiar que retrata a banda.
         </p>
@@ -27,17 +29,18 @@ $cache = time();
             “A música dos DUL é uma experiência singular através de temas que sugerem vários tipos de sentimentos. Visitam a alegria e a intervenção, com temas como a “Marcha do Povo Sangra” e “Fun&Farra”, abraça com bastante força a intensidade de uma viagem tribal, com temas como a “Pássaro de Corda”, e se recolhem novamente na introspeção sobre o amor, em temas como a “About a Boy” e o "Amor Epitelial". Quase no final desta eclética viagem emocional, dão também a provar uma pequena dose de saudosismo e nostalgia que se escondem, inocentemente, por detrás de interpretações muito próprias de temas como a “Gaivota”, de Amália Rodrigues, e “O Prelúdio Para a Laurindinha”, tema que junta a interpretação do conhecido tema "Laurindinha” de António Mourão, com um prelúdio criado pela própria banda.” (in press)
         </p>
     </article>
+    <footer>
+        <a  href="https://www.facebook.com/dulandnoukwhite">
+            <ion-icon name="logo-facebook"></ion-icon>
+        </a>
+        <a href="https://www.instagram.com/dulandnoukwhiteband/">
+            <ion-icon name="logo-instagram"></ion-icon>
+        </a>
+        <a href="https://www.youtube.com/c/DulNNoukWhite">
+            <ion-icon name="logo-youtube"></ion-icon>
+        </a>
+    </footer>
 </main>
-<footer>
-    <a  href="https://www.facebook.com/dulandnoukwhite">
-        <ion-icon name="logo-facebook"></ion-icon>
-    </a>
-    <a href="https://www.instagram.com/dulandnoukwhiteband/">
-        <ion-icon name="logo-instagram"></ion-icon>
-    </a>
-    <a href="https://www.youtube.com/c/DulNNoukWhite">
-        <ion-icon name="logo-youtube"></ion-icon>
-    </a>
-</footer>
+
 </body>
 </html>
